015c6b2383ca7095593f597f67ad112a6a5c1bc2
TM Revanza <revanzanrndra@gmail.com>
2022-09-09 23:23:17 +0700

    instruksi 1-3 done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    catalog_item_data = CatalogItem.objects.all()
+    context = {
+        'list_item': catalog_item_data,
+        'nama': 'TM Revanza Narendra Pradipta',
+        'npm': '2206025003'
+    }
+    return render(request, "katalog.html", context)
